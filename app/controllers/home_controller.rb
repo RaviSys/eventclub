947125d8fd77d97c0a8f626974e5829f33e49305
Ravi Kumar Singh <rksingh@isystango.com>
2019-02-27 19:47:11 +0530

    working on event search featires

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,9 @@ class HomeController < ApplicationController
   end
 
   def search_events
+    lat = params[:search][:latitude]
+    long = params[:search][:longitude]
+    @events = Event.near([lat, long], 50, units: :km)
   end
 
 end
